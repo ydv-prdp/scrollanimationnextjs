7897e8be6404f1128c1b2a4e7eba6a360773d420
Pradeep Yadav <pradeepyadav101010@gmail.com>
2023-06-24 22:40:58 +0530

    added text section with animation and video section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
 'use client'
-import Image from 'next/image'
-import styles from './page.module.css'
+
 import Header from './components/Header/Header'
 import { styled } from 'styled-components'
 import SectionLayout from './components/SectionLayout'
 import Card from "./components/Card";
 import { cards } from "./utils/cards";
 import Fullpage from './components/Fullpage'
+import TextSection from './TextSection'
+import { motion, useScroll, useTransform } from "framer-motion";
+import React from 'react'
 
 export default function Home() {
+  const video = React.useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: video,
+    offset: ["start end", "end start"],
+  });
+
+  const opacity = useTransform(scrollYProgress, [0, 0.65, 1], [1, 1, 0]);
+  const scale = useTransform(
+    scrollYProgress,
+    [0, 0.6, 0.8, 0.9],
+    [1, 0.8, 0.8, 0]
+  );
   return (
     <>
       <Header/>
@@ -28,6 +42,41 @@ export default function Home() {
             </div>
         </SectionLayout>
         <Fullpage/>
+        <SectionLayout>
+        <div className="cards">
+              {cards.map((card, index) => {
+                return (
+                  <Card
+                    key={index}
+                    title={card.title}
+                    description={card.description}
+                    image={card.image}
+                  />
+                );
+              })}
+            </div>
+        </SectionLayout>
+        <SectionLayout>
+          <TextSection/>
+        </SectionLayout>
+
+        <SectionLayout>
+          <motion.div
+            className="video"
+            ref={video}
+            style={{
+              opacity,
+              scale,
+            }}
+          >
+            <iframe
+              src="https://www.youtube.com/embed/OuaUjkZhfqQ"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </motion.div>
+        </SectionLayout>
       </MainStyled>
     </>
   )
